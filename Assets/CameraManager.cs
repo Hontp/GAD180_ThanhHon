68c04303bfe81d125a8dfdb94c29701acbba66af
Krystal <1015590@student.sae.edu.au>
2019-05-30 19:29:08 +0800

    Camera Tracking Start

---
+++ Assets/CameraManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+
+    public Rigidbody2D focusTarget;
+    private Vector2 lookOffset;
+
+    public float maxHorizontalOffset;
+    public float maxVerticalOffset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+        transform.position = new Vector3(focusTarget.transform.position.x,focusTarget.transform.position.y,-10f);
+    }
+}
