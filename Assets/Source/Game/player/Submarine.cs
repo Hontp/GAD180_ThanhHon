02389c780b188561b63d218ec8f3c377995c8a0f
Hontp <puke_475@hotmail.com>
2019-06-13 16:44:01 +0800

    Fixed Scrambled ship parts
    * ship now in the correct order

--- Assets/Source/Game/player/Submarine.cs
+++ Assets/Source/Game/player/Submarine.cs
@@ -7,9 +7,9 @@ public class Submarine : Ship
     public override void Initialize()
     {
         // load sprites for sub components
-        SetSprite("hull", "Art/sub_hull");
-        SetSprite("weapon", "Art/sub_weapon");
-        SetSprite("propulsion", "Art/sub_propulsion");
+        SetSprite("hull", "Art/shipParts/hull_1");
+        SetSprite("weapon", "Art/shipParts/Weap_1");
+        SetSprite("propulsion", "Art/shipParts/Prop_1");
 
         // scale the submarine
         transform.localScale = new Vector3(0.25f, 0.25f, 0.25f);
