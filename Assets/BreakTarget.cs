c6a541d0daee6da0d964d501e35a2cbc22490dbd
Krystal McDonald <krystalmc478@gmail.com>
2019-07-02 17:34:31 +0800

    added a target break prefab

--- Assets/BreakTarget.cs
+++ Assets/BreakTarget.cs
@@ -6,10 +6,8 @@ public class BreakTarget : MonoBehaviour
 {
     // Start is called before the first frame update
     private Rigidbody2D rb;
-    [SerializeField]
-    private float lifetime;
-    [SerializeField]
-    private float lifeStart;
+    public float lifetime;
+    public float lifeStart;
 
     [SerializeField]
     private Vector2 startVelocity;
@@ -21,11 +19,19 @@ public class BreakTarget : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         rb.AddForce(startVelocity,ForceMode2D.Impulse);
         rb.AddTorque(rotationVelocity,ForceMode2D.Impulse);
+        lifeStart = Time.time;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float normalizedLife = (Time.time - lifeStart / lifetime);
+        //shrinking effect
+        float scale = 1f - normalizedLife;
+        if(scale < 0 )
+        {
+          //  Destroy(gameObject);
+        }
+        transform.localScale *= scale; 
     }
 }
