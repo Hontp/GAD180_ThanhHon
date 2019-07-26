4192646c77ec626c3b7a0cd28874039a03f52c0d
Daedalus13 <51198118+Daedalus13@users.noreply.github.com>
2019-07-26 15:22:31 +0800

    Dans First Push

---
+++ Assets/scripts/ImageSelection.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class ImageSelection : MonoBehaviour
+{
+    public Image SelectionImage;
+    public List<Sprite> ItemList = new List<Sprite>();
+    private int itemSpot = 0;
+
+    public void RightSelection()
+    {
+        Debug.Log("Right");
+        if (itemSpot < ItemList.Count - 1)
+        {            
+            itemSpot++;
+            SelectionImage.sprite = ItemList[itemSpot];
+        }
+    }
+
+    public void LeftSelection()
+    {
+        Debug.Log("Left");
+        if (itemSpot > 0)
+        {
+            itemSpot--;
+            SelectionImage.sprite = ItemList[itemSpot];
+        }
+    }
+
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
